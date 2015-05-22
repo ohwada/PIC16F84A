8419f127901a76b2b7efd1400f1ff2393fbcac5f
Kenichi Ohwada <ml.ohwada@gmail.com>
2015-05-23 00:49:02 +0900

    asm

--- code/asm_led/main.asm
+++ code/asm_led/main.asm
@@ -1,87 +1,87 @@
-; ====================
+; **************************************************
 ;   PIC16F84A LED Blink
 ;   2015-04-26 K.OHWADA
 ;
 ;   CLOCK   4MHz ( 1 us / cycle )
 ;   PORTA.RA2   LED
-; ====================
+; **************************************************
 
 ; ==========
 ; CPU Definition
 ; ==========
-    LIST        P=PIC16F84A
-    INCLUDE P16F84A.INC
+    list            p=16F84A            ; list directive to define processor
+    #include    <p16F84A.inc>   ; processor specific variable definitions
 
 ; ==========
 ; CPU Configuraton
-;   HS Oscillator
-;   Watchdog Timer disable
-;   Power-up Timer eable
 ;   Code Protection disable
+;   Power-up Timer eable
+;   Watchdog Timer disable
+;   HS Oscillator
 ; ==========
-    __CONFIG _HS_OSC & _WDT_OFF & _PWRTE_ON & _CP_OFF
+    __CONFIG  _CP_OFF & _PWRTE_ON & _WDT_OFF & _HS_OSC
 
 ; ==========
+; VARIABLE DEFINITIONS
 ; SRAM (General Purpose Registers) 0Ch - 4Fh
 ; ==========
-D1CNT   EQU     0Ch	
-D2CNT   EQU     0Dh
-D3CNT   EQU     0Eh
+d1cnt   EQU     0x0C	
+d2cnt   EQU     0x0D
+d3cnt   EQU     0x0E
 
 ; ==========
 ; Program
 ; ==========
-; Reset
-    ORG      0 
-    GOTO    MAIN 
-; Interrupt
-;   ORG      4
-;   GOTO    INTERRUPT
+; processor reset vector
+    ORG      0x000 
+    goto    Main 
+; interrupt vector location
+;   ORG      0x004
 
-MAIN:
-    BSF      STATUS,RP0  ; bunk 1
-    CLRF    TRISA           ; PortA all output
-    CLRF    TRISB           ; PortB all output
-    BCF      STATUS,RP0   ; bunk 0
-    CLRF PORTA ; PortA all low
-    CLRF PORTB ; PortB all low
+Main:
+    bsf    STATUS,RP0  ; bunk 1
+    clrf    TRISA           ; PortA all output
+    clrf    TRISB           ; PortB all output
+    bcf    STATUS,RP0   ; bunk 0
+    clrf    PORTA       ; PortA all low
+    clrf    PORTB       ; PortB all low
 
-LOOP:
-    BSF          PORTA,2       ; RA2 on
-    CALL        DELAY3        ; delay 1 sec
-    BCF          PORTA,2       ; RA2 off
-    CALL        DELAY3        ; delay 1 sec
-    GOTO       LOOP
+Loop:
+    bsf        PORTA,2       ; RA2 on
+    call        Delay3        ; delay 1 sec
+    bcf        PORTA,2       ; RA2 off
+    call        Delay3        ; delay 1 sec
+    goto       Loop
 
 ; Delay 1,000 us = 1 us x 4 cycle x 250 	
-DELAY1:			
-    MOVLW       0FAh	
-    MOVWF       D1CNT   ; 0FAh(250) -> D1CNT
-D1LOOP:   
-    NOP                         ; 1 cycle		
-    DECFSZ      D1CNT,F ; 1 cycle : D1CNT --
-    GOTO	 D1LOOP  ; 2 cycle	
-    RETURN			
+Delay1:			
+    movlw       0xfa	
+    movwf       d1cnt   ; 0xfa(250) -> d1cnt
+D1Loop:   
+    nop                         ; 1 cycle		
+    decfsz      d1cnt,F ; 1 cycle : d1cnt --
+    goto	 D1Loop  ; 2 cycle	
+    return			
 
 ; Delay 250 ms = 1 ms x 250 
-DELAY2:			
-    MOVLW       0FAh		
-    MOVWF       D2CNT   ; 0FAh(250) -> D2CNT		
-D2LOOP: 
-    CALL           DELAY1 ; 1 ms	
-    DECFSZ       D2CNT,F ; D2CNT --
-    GOTO         D2LOOP	
-    RETURN				
+Delay2:			
+    movlw       0xfa		
+    movwf       d2cnt   ; 0xfa(250) -> d2cnt		
+D2Loop: 
+    call           Delay1 ; 1 ms	
+    decfsz       d2cnt,F ; d2cnt --
+    goto         D2Loop	
+    return				
 
 ; Delay 1,000 ms = 250 ms x 4		
-DELAY3:			
-    MOVLW       003h		
-    MOVWF	  D3CNT    ; 004h(4) -> D3CNT		
-D3LOOP:  
-    CALL          DELAY2    ; 250 ms		
-    DECFSZ	  D3CNT,F ; D3CNT --
-    GOTO	  D3LOOP			
-    RETURN				
+Delay3:			
+    movlw       0x04		
+    movwf	  d3cnt    ; 0x04(4) -> d3cnt		
+D3Loop:  
+    call          Delay2    ; 250 ms		
+    decfsz	  d3cnt,F ; d3cnt --
+    goto	  D3Loop			
+    return				
 
     END
 ; ==========
